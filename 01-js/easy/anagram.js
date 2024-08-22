df481a654b5fa0ecdfd1682648b6ff2073d70960
kunal chugh <157191243+kunal-v2@users.noreply.github.com>
2024-08-22 12:02:15 +0530

    Updated anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,15 @@
 */
 
 function isAnagram(str1, str2) {
-
+  const cleanStr1 = str1.replace(/[^\w]/g, "");
+  const cleanStr2 = str2.replace(/[^\w]/g, "");
+  if (
+    cleanStr1.split("").sort().join("") === cleanStr2.split("").sort().join("")
+  ) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
 }
-
+isAnagram("listen", "silent");
 module.exports = isAnagram;
