d9ac6a5ae9df597a30a6a45ec32a50e747479c02
kunal chugh <157191243+kunal-v2@users.noreply.github.com>
2024-08-22 12:23:58 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,13 @@
 */
 
 function isAnagram(str1, str2) {
-
+  const cleanStr1 = str1.replace(/[^a-z]/gi, '').toLowerCase();
+  const cleanStr2 = str2.replace(/[^a-z]/gi, '').toLowerCase();
+  if (cleanStr1.split('').sort().join('') === cleanStr2.split('').sort().join('')) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
 }
-
+isAnagram('steal', 'steel'); // This should output false
 module.exports = isAnagram;
