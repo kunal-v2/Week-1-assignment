a16c6dd15d5dd239b7bd24a31688fd0627acf826
kunal chugh <157191243+kunal-v2@users.noreply.github.com>
2024-08-22 12:38:55 +0530

    Update times.js
    assignment completed

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,16 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+  const startTime = new Date().getTime();
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  const endTime = new Date().getTime();
+  const timeTaken = (endTime - startTime) / 1000;
+  console.log(`Time taken to calculate the sum from 1 to ${n} is: ${timeTaken} seconds`);
+  return timeTaken;
 }
+calculateTime(599);
+calculateTime(100000);   
+calculateTime(1000000000);  
